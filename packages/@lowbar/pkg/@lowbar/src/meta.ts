e278c83f8d0972284c870294f131656a733356fd
@ <@>
2018-03-12 12:59:51 +0100

    lowbar: Implin untested `*range/*till` iteration helpers; cli.preparse: Dependin on former

--- packages/@lowbar/pkg/@lowbar/src/meta.ts
+++ packages/@lowbar/pkg/@lowbar/src/meta.ts
@@ -25,6 +25,39 @@ export function isStr(a :any) :a is string {
 }
 
 // ---
-export function range() {
-    //TODO
+
+export type Rangable = Int | string
+export function *range<
+      Origin extends Rangable>(
+    origin :Origin,
+) {
+    const rawMap :{
+        [key :string] :<Clothed extends Rangable>(clothed :Clothed) => Int
+    } = {
+        number: (clo) => Math.floor(clo as Int) as Int,
+        string: (clo) => (clo as string).codePointAt(0) as Int,
+    }
+    const clothMap :{
+        [key :string] :(raw :Int) => Rangable
+    } = {
+        number: (raw) => raw,
+        string: (raw) => String.fromCodePoint(raw),
+    }
+
+    const ty = typeof origin
+    const rawOri = rawMap[ty](origin)
+
+    let raw: number = rawOri, step :Int; 
+    for (
+        ;
+        ;raw+=step as number
+    ) step = yield clothMap[ty](raw as Int)
+}
+export function *till<T>(
+    it :Iterable<T>,
+    fin :T,
+) {
+    for (let e of it) if (fin === e)
+        return fin
+    else yield e
 }
