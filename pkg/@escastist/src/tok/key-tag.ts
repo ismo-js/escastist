d31b627c63ff39f85953cd17db4c57274323ad56
probit @ 3520 <probitlabs@protonmail.com>
2018-03-26 07:43:56 +0200

    escastist: Initin key tag tokenizer

---
+++ pkg/@escastist/src/tok/key-tag.ts
@@ -0,0 +1,12 @@
+export const map = {
+    "break": a.BREAK,
+
+    "case": a.CASE,
+    "catch": a.CATCH,
+    "class": u.CLASS,
+    "continue": a.CONTINUE,
+    "const": a.CONST,
+
+    "debugger": a.DEBUGGER,
+    "default": a.DEFAULT,
+}
