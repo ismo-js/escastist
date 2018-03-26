51aa3c7d43c01384d4c6c0fd348a76c64dbb876f
probit @ 3520 <probitlabs@protonmail.com>
2018-03-27 00:20:36 +0200

    escastist: Initin basic latin map

--- pkg/@escastist/src/char-poi/basic-latin.ts
+++ pkg/@escastist/src/char-poi/basic-latin.ts
@@ -1,3 +1,35 @@
-export default class BasicLatin {
-    @to() [0x00] = Symbol() //TODO
+import {sym} from "@beyond-life/lowbar"
+
+// ~~~
+
+const poiKinds = sym("Code point kind")
+export const WHITE_SPACE :unique symbol =
+    Symbol(poiKinds.WHITE_SPACE.toString())
+export const LINE_TERM :unique symbol =
+    Symbol(poiKinds.LINE_TERM.toString())
+
+export type PoiKind = never
+    | typeof WHITE_SPACE
+    | typeof LINE_TERM
+
+export interface PoiMetadata {
+    kind :PoiKind
+    name :string
+}
+
+export interface PoiMetaset {
+    [key :number] :PoiMetadata
+}
+
+export default class BasicLatin
+      implements PoiMetaset {
+    ;[0x09] :PoiMetadata = {
+        kind: WHITE_SPACE,
+        name: "Tab",
+    }
+    ;[0x0b] :PoiMetadata = {
+        kind: WHITE_SPACE,
+        name: "VTab",
+    }
+
 }
