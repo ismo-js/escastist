6d59e5c9ec09873d3e38c13ccd7e2bce3c24bdbb
probit @ 3520 <probitlabs@protonmail.com>
2018-03-31 17:49:08 +0200

    *: Bootstrappin, c-lex: Initin

--- pkg/@core-lex/src/meta.ts
+++ pkg/@core-lex/src/meta.ts
@@ -1,11 +1,27 @@
 import {
+    Stream as $,
+    MemoryStream as Mem$,
+} from "xstream"
+import flattenSeq from "xstream/extra/flattenSequentially"
+
+// @@@
+
+import {
     Int, isInt,
 } from "@beyond-life/lowbar"
 
 // ~~~
 
 export default function lex(
-
+    chunk$ :$<string>,
 ) {
+    const poi$$ = chunk$.map((e :string) :$<Int> => {
+        const ePois = [...e].map((poi :string) :Int =>
+            poi.codePointAt(0) as Int
+        )
+        const ePoi$ = $.from(ePois)
 
+        return ePoi$
+    })
+    const poi$ = poi$$.compose(flattenSeq)
 }
