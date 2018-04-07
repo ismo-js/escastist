cda739ac9ae9a141100bcc07e0f63d1e9cbbaff2
probit @ 3520 <probitlabs@protonmail.com>
2018-04-08 00:17:15 +0200

    @sigma.meta: Dependin `staltz/xstream` → driver

--- pkg/aurora/@sigma/src/meta.ts
+++ pkg/aurora/@sigma/src/meta.ts
@@ -1,5 +1,29 @@
+import {Stream as $} from "xstream"
 import {adapt} from "@cycle/run/lib/adapt"
 
+// @@@
+
+import {Int} from "@escastist/lowbar-prim"
+
+// ~~~
+
+export interface Resrc {
+    resourceUri :string
+    uuid :string
+}
+
+export interface Server {
+    context :boolean
+    cpu :Int
+}
+
 export function sigma(out$) {
+    const rest :{
+        name :string
+        owner :Resrc
+    } & Resrc = {} as any
+
+    const in$ :$<typeof rest> = {} as any
+
     return adapt(in$)
 }
